ced6c70527315e8c88cd086fa809a13cb5ea4025
netpair-Dhruvik <dhruvik.shah@netpairInfoTech.com>
2023-02-09 19:19:50 +0530

    Banner 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
-import Banner from "./components/banner";
-import Banner3 from "./components/banner3";
-import Banner4 from "./components/banner4";
-import Header from "./components/header";
+import { useState } from 'react'
+import Banner from './components/banner'
+import Banner3 from './components/banner3'
+import Banner4 from './components/banner4'
+import Card from './components/card'
+import Header from './components/header'
+import Act1 from '../src/assets/images/Group20.png'
+import Banner5 from './components/banner5'
 
 function App() {
   return (
-    <><section className="bg min-h-screen">
-      <Header />
-      <Banner />
-      <Banner3 />
-      <Banner4 />
-    </section>
+    <>
+      <section className="bg min-h-screen">
+        <Header />
+        <Banner />
+        <Banner3 />
+        <Banner4 />
+        <Banner5 />
+      </section>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/banner3.jsx
+++ src/components/banner3.jsx
@@ -4,56 +4,60 @@ import banner4Logo from '../assets/images/banner4Logo.png'
 import Button from '../components/button'
 
 function Banner3() {
-    return (
-        <>
-            <div className='min-h-screen bg2'>
-                <section className="container mx-auto flex flex-col justify-center items-center md:pb-40 pb-20">
-                    <div className="flex flex-col items-center justify-center gap-3 md:flex-row">
-                        <div className="flex flex-col justify-center items-center gap-5 px-3 py-5 md:items-start md:flex-1">
-                            <h1 className="text-bg text-center text-xl uppercase font-bold md:text-start md:text-3xl lg:text-5xl">
-                                Customer Support
-                            </h1>
-                            <h2 className=" text-white md:text-2xl lg:text-4xl text-center md:text-start">Get In Touch With Zetbet  CUSTOMER CARE For Any Queries, Emergencies, Feedbacks or Complaints. We Are Here To Help You 24/7 With Our Online Services.
-                                24 hours service</h2>
-                            <h2 className=" text-white md:text-xl lg:text-3xl text-center md:text-start">
-                                Customer Care No For WhatsApp & Calling
-                            </h2>
-                            <div className="flex flex-col sm:flex-row gap-5">
-                                <Button />
-                                <Button />
-                            </div>
-                        </div>
-                        <div className="px-3 h-38 sm:flex-1">
-                            <img src={banner3Logo} alt="" srcSet="" />
-                        </div>
-                    </div>
-                </section>
-
-                <section className="container mx-auto flex flex-col justify-center items-center md:pb-40 pb-20">
-                    <div className="flex flex-col items-center justify-center gap-3 md:flex-row">
-                        <div className="px-3 h-38 sm:flex-1">
-                            <img src={banner4Logo} alt="" srcSet="" />
-                        </div>
-                        <div className="flex flex-col justify-center items-center gap-5 px-3 py-5 md:items-start md:flex-1">
-                            <h1 className="text-bg text-center text-xl uppercase font-bold md:text-start md:text-3xl lg:text-5xl">
-                                Place your BET in our IDs
-                            </h1>
-                            <h2 className=" text-white md:text-2xl lg:text-4xl text-center md:text-start">With Our Step-By-Step Guide For Beginners,
-                                Get Started Playing. Start Learning Now.. 24/7 ACTIVE.</h2>
-                            <h2 className=" text-white md:text-xl lg:text-3xl text-center md:text-start">
-                                We deals only on WhatsApp
-                            </h2>
-                            <div className="flex flex-col sm:flex-row gap-5">
-                                <Button />
-                                <Button />
-                            </div>
-                        </div>
-
-                    </div>
-                </section>
+  return (
+    <>
+      <div className="min-h-screen">
+        <section className="container mx-auto flex flex-col justify-center items-center md:pb-40 pb-20">
+          <div className="flex flex-col items-center justify-center gap-3 md:flex-row">
+            <div className="flex flex-col justify-center items-center gap-5 px-3 py-5 md:items-start md:flex-1">
+              <h1 className="text-bg text-center text-xl uppercase font-bold md:text-start md:text-3xl lg:text-5xl">
+                Customer Support
+              </h1>
+              <h2 className=" text-white md:text-2xl lg:text-4xl text-center md:text-start">
+                Get In Touch With Zetbet CUSTOMER CARE For Any Queries,
+                Emergencies, Feedbacks or Complaints. We Are Here To Help You
+                24/7 With Our Online Services. 24 hours service
+              </h2>
+              <h2 className=" text-white md:text-xl lg:text-3xl text-center md:text-start">
+                Customer Care No For WhatsApp & Calling
+              </h2>
+              <div className="flex flex-col sm:flex-row gap-5">
+                <Button />
+                <Button />
+              </div>
             </div>
-        </>
-    )
+            <div className="px-3 h-38 sm:flex-1">
+              <img src={banner3Logo} alt="" srcSet="" />
+            </div>
+          </div>
+        </section>
+
+        <section className="container mx-auto flex flex-col justify-center items-center md:pb-40 pb-20">
+          <div className="flex flex-col items-center justify-center gap-3 md:flex-row">
+            <div className="px-3 h-38 sm:flex-1">
+              <img src={banner4Logo} alt="" srcSet="" />
+            </div>
+            <div className="flex flex-col justify-center items-center gap-5 px-3 py-5 md:items-start md:flex-1">
+              <h1 className="text-bg text-center text-xl uppercase font-bold md:text-start md:text-3xl lg:text-5xl">
+                Place your BET in our IDs
+              </h1>
+              <h2 className=" text-white md:text-2xl lg:text-4xl text-center md:text-start">
+                With Our Step-By-Step Guide For Beginners, Get Started Playing.
+                Start Learning Now.. 24/7 ACTIVE.
+              </h2>
+              <h2 className=" text-white md:text-xl lg:text-3xl text-center md:text-start">
+                We deals only on WhatsApp
+              </h2>
+              <div className="flex flex-col sm:flex-row gap-5">
+                <Button />
+                <Button />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  )
 }
 
-export default Banner3;
+export default Banner3
